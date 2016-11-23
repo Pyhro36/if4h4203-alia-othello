efd24ab3a005f119181bf486a83a4c1d01a9742d
fadwa.messaoudi <fadwa.messaoudi@gmail.com>
2016-11-23 10:33:49 +0100

    runnable game

--- script.pl
+++ script.pl
@@ -25,4 +25,4 @@ contactBasGauche(X,Y,X2,Y2) :- X2 is X-1, Y2 is Y+1.
 contactHautDroite(X,Y,X2,Y2) :- X2 is X+1, Y2 is Y-1.
 contactBasDroite(X,Y,X2,Y2) :- X2 is X+1, Y2 is Y+1.
 
-vide(X, Y) :- 
+vide(X, Y) :- not(case(X,Y,_)).
